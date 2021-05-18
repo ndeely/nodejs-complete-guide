2acaefb73bc8bbccf1b3995d1db72905d52840ed
Niall Deely <nialldeely@gmail.com>
2021-05-18 15:17:56 +0100

    added ejs for templating
    - EJS module added for templating
    - converted static HTML pages to dynamic EJS files

--- routes/admin.js
+++ routes/admin.js
@@ -5,14 +5,23 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+const products = [];
 
+router.get('/add-product', (req, res, next) => {
+    res.render(
+        'add-product',
+        {
+            prods: products,
+            pageTitle: 'Add Product',
+            path: '/admin/add-product'
+        }
+    );
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push( { title: req.body.title } );
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
