61eda22c525deca01ed804319596cfe91e4e3666
Niall Deely <nialldeely@gmail.com>
2021-06-10 15:12:19 +0100

    fixed cart functionality
    - renamed products controller to shop controller
    - added cart functionality to user model (add/delete cart item)
    - added cart css
    - updated cart view

--- models/user.model.js
+++ models/user.model.js
@@ -59,7 +59,45 @@ class User {
                 );
     }
 
+    deleteFromCart(prodId) {
+        const updatedCartItems = this.cart.items.filter(i => {
+            return i.productId.toString() !== prodId.toString();
+        });
+        const db = getDb();
+        return db
+            .collection('users')
+            .updateOne(
+                { _id: new mongodb.ObjectId( this.id ) },
+                { $set: { cart: { items: updatedCartItems } } }
+            );
+    }
+
+    getCart() {
+        const db = getDb();
+        const productIds = this.cart.items.map(
+          i => {
+              return i.productId;
+          }
+        );
+        return db
+            .collection('products')
+            .find({ _id: { $in: productIds } })
+            .toArray()
+            .then(products => {
+                return products
+                    .map(p => {
+                        return {
+                            ...p,
+                            quantity: this.cart.items.find(i => {
+                            return i.productId.toString() === p._id.toString();
+                            }).quantity
+                        };
+                    });
+            });
+    }
 }
+
+
 
 
 module.exports = User;
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,16 @@
 const express = require('express');
 
-const productsController = require('../controllers/products.controller');
+const shopController = require('../controllers/shop.controller');
 
 const router = express.Router();
 
-router.get('/', productsController.getHomepage);
-router.get('/products', productsController.getProducts);
-router.get('/products/:id', productsController.getProduct);
-router.get('/cart', productsController.getCart);
-router.post('/cart', productsController.postCart);
-// router.get('/orders', productsController.getOrders);
-// router.post('/cart-delete-item', productsController.deleteCartItem);
-// router.post('/create-order', productsController.postOrder);
+router.get('/', shopController.getHomepage);
+router.get('/products', shopController.getProducts);
+router.get('/products/:id', shopController.getProduct);
+router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
+// router.get('/orders', shopController.getOrders);
+router.post('/cart-delete-item', shopController.deleteCartItem);
+// router.post('/create-order', shopController.postOrder);
 
 module.exports = router;
