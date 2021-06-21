88d1e0ac801af6ed16efc860ec88f1b4fe5e913b
Niall Deely <nialldeely@gmail.com>
2021-06-21 17:38:33 +0100

    fixed email validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { body, check } = require('express-validator');
 
 const authController = require('../controllers/auth.controller');
 const isAuth = require('../middleware/is-auth');
@@ -13,22 +13,26 @@ router.post('/login', authController.postLogin);
 router.post('/logout', isAuth, authController.postLogout);
 router.get('/signup', authController.getSignup);
 router.post('/signup', [
-    check('email')
-        .isEmail()
-        .withMessage("Invalid Email Address")
-        .custom((value, { req }) => {
-            User.findOne({ email: value })
-                .then(userDoc => {
-                   if (userDoc) return false;
-                });
-        })
-        .withMessage("This email address is already in use"),
-    check('password')
+    body('email', 'Invalid Email Address').exists().isEmail().trim().escape()
+        .custom(email => {
+            return new Promise((resolve, reject) => {
+                User.findOne({ email: email })
+                    .then(user => {
+                        if (user !== null) {
+                            reject(new Error('This email address is already in use'))
+                        } else {
+                            resolve(true)
+                        }
+                    })
+
+            });
+        }),
+    body('password')
         .isLength({min: 5})
         .withMessage("Password must be at least 5 characters long"),
-    check('confirmPassword')
-        .custom((value, { req }) => {
-            if (value === req.body.password) return true;
+    body('confirmPassword')
+        .custom((confirmPassword, { req }) => {
+            if (confirmPassword === req.body.password) return true;
         })
         .withMessage("Passwords do not match")
 ], authController.postSignup);
