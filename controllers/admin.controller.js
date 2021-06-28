dc3fc664e693bed6136a6e62ae79b9de099045c7
Niall Deely <nialldeely@gmail.com>
2021-06-28 14:39:06 +0100

    delete product changed to async request
    delete product no longer reloads the page, but deletes the product from the database and the DOM

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -224,22 +224,24 @@ exports.getProducts = (req, res, next) => {
         } });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId)
         .then(p => {
             if (!p) {
                 return next(new Error('Product not found'));
             }
-            fileHelper.deleteFile(p.imageUrl);
+            fileHelper.deleteFile('.' + p.imageUrl);
             return Product.deleteOne({_id: prodId, userId: req.user._id});
         })
         .then(() => {
-            res.redirect('/admin/products');
+            res.status(200).json({
+                message: 'Success!'
+            });
         })
         .catch(err => { if (err) {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            return next(error);
+            res.status(500).json({
+                message: 'Deleting product failed'
+            });
         } });
 };
